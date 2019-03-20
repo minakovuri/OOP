0f246ffcd22fcefad78dbec9f90132da7277c917
minakov_uri <minakov_uri@mail.ru>
2019-03-21 02:13:23 +0300

    Изменил кодировку файла с тестами RemoteControl

--- Labs/Lab3/task1/Car/Remote_tests/Remote_tests.cpp
+++ Labs/Lab3/task1/Car/Remote_tests/Remote_tests.cpp
@@ -5,7 +5,7 @@
 
 void VerifyCommand(CRemoteControl& remoteControl, std::stringstream& input, std::stringstream& output, const std::string& command, const std::string& expectedOutput)
 {
-	/* Очищаем содиржимое потоков */
+	/* РћС‡РёС‰Р°РµРј СЃРѕРґРёСЂР¶РёРјРѕРµ РїРѕС‚РѕРєРѕРІ */
 	output = std::stringstream();
 	input = std::stringstream();
 
@@ -25,76 +25,76 @@ TEST_CASE("Test how CRemoteControl instance work")
 
 	CRemoteControl remoteControl(car, input, output);
 
-	/* Проверяем начальное состояние машины */
+	/* РџСЂРѕРІРµСЂСЏРµРј РЅР°С‡Р°Р»СЊРЅРѕРµ СЃРѕСЃС‚РѕСЏРЅРёРµ РјР°С€РёРЅС‹ */
 	VerifyCommand(remoteControl, input, output, "Info", "Car info:\n\tEngine state: Off\n\tDirection: Not moving\n\tSpeed: 0\n\tGear: 0\n");
 
 
 
-	/* Включаем двигатель и проверяем сообщение */
+	/* Р’РєР»СЋС‡Р°РµРј РґРІРёРіР°С‚РµР»СЊ Рё РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "EngineOn", "Engine is turned on\n");
 
-	/* Пробуем включить двигатель еще раз, проверяем сообщение о невозможности сделать это */
+	/* РџСЂРѕР±СѓРµРј РІРєР»СЋС‡РёС‚СЊ РґРІРёРіР°С‚РµР»СЊ РµС‰Рµ СЂР°Р·, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ Рѕ РЅРµРІРѕР·РјРѕР¶РЅРѕСЃС‚Рё СЃРґРµР»Р°С‚СЊ СЌС‚Рѕ */
 	VerifyCommand(remoteControl, input, output, "EngineOn", "Can not turn engine on\n");
 
-	/* Проверяем состояние машины */
+	/* РџСЂРѕРІРµСЂСЏРµРј СЃРѕСЃС‚РѕСЏРЅРёРµ РјР°С€РёРЅС‹ */
 	VerifyCommand(remoteControl, input, output, "Info", "Car info:\n\tEngine state: On\n\tDirection: Not moving\n\tSpeed: 0\n\tGear: 0\n");
 
 
 
-	/* Переключаем передачу на первую, проверяем сообщение */
+	/* РџРµСЂРµРєР»СЋС‡Р°РµРј РїРµСЂРµРґР°С‡Сѓ РЅР° РїРµСЂРІСѓСЋ, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "SetGear 1", "Gear is shifted to 1\n");
 
-	/* Пробуем переключить передачу на вторую, проверяем сообщение */
+	/* РџСЂРѕР±СѓРµРј РїРµСЂРµРєР»СЋС‡РёС‚СЊ РїРµСЂРµРґР°С‡Сѓ РЅР° РІС‚РѕСЂСѓСЋ, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "SetGear 2", "Can not shift gear to 2\n");
 
-	/* Проверяем состояние машины */
+	/* РџСЂРѕРІРµСЂСЏРµРј СЃРѕСЃС‚РѕСЏРЅРёРµ РјР°С€РёРЅС‹ */
 	VerifyCommand(remoteControl, input, output, "Info", "Car info:\n\tEngine state: On\n\tDirection: Not moving\n\tSpeed: 0\n\tGear: 1\n");
 
 
 
-	/* Разгоняем машину, проверяем сообщение */
+	/* Р Р°Р·РіРѕРЅСЏРµРј РјР°С€РёРЅСѓ, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "SetSpeed 25", "Speed is changed to 25\n");
 
-	/* Пытаемся разгонать машину до недопутимой на данной передаче скорости, проверяем сообщение */
+	/* РџС‹С‚Р°РµРјСЃСЏ СЂР°Р·РіРѕРЅР°С‚СЊ РјР°С€РёРЅСѓ РґРѕ РЅРµРґРѕРїСѓС‚РёРјРѕР№ РЅР° РґР°РЅРЅРѕР№ РїРµСЂРµРґР°С‡Рµ СЃРєРѕСЂРѕСЃС‚Рё, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "SetSpeed 35", "Can not change speed to 35\n");
 
-	/* Проверяем состояние машины */
+	/* РџСЂРѕРІРµСЂСЏРµРј СЃРѕСЃС‚РѕСЏРЅРёРµ РјР°С€РёРЅС‹ */
 	VerifyCommand(remoteControl, input, output, "Info", "Car info:\n\tEngine state: On\n\tDirection: Not moving\n\tSpeed: 25\n\tGear: 1\n");
 
 
 
-	/* Пытаемся переключиться на заднюю передачу с ненулевой скорости, проверяем сообщение */
+	/* РџС‹С‚Р°РµРјСЃСЏ РїРµСЂРµРєР»СЋС‡РёС‚СЊСЃСЏ РЅР° Р·Р°РґРЅСЋСЋ РїРµСЂРµРґР°С‡Сѓ СЃ РЅРµРЅСѓР»РµРІРѕР№ СЃРєРѕСЂРѕСЃС‚Рё, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "SetGear -1", "Can not shift gear to -1\n");
 
 
 
-	/* Переключаем передачу на вторую, проверяем сообщение */
+	/* РџРµСЂРµРєР»СЋС‡Р°РµРј РїРµСЂРµРґР°С‡Сѓ РЅР° РІС‚РѕСЂСѓСЋ, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "SetGear 2", "Gear is shifted to 2\n");
 
 
 
-	/* Переключаем передачу на нейтральную, проверяем сообщение */
+	/* РџРµСЂРµРєР»СЋС‡Р°РµРј РїРµСЂРµРґР°С‡Сѓ РЅР° РЅРµР№С‚СЂР°Р»СЊРЅСѓСЋ, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "SetGear 0", "Gear is shifted to 0\n");
 
-	/* Пробуем разогнать машину на нейтральной передаче, проверяем сообщение */
+	/* РџСЂРѕР±СѓРµРј СЂР°Р·РѕРіРЅР°С‚СЊ РјР°С€РёРЅСѓ РЅР° РЅРµР№С‚СЂР°Р»СЊРЅРѕР№ РїРµСЂРµРґР°С‡Рµ, РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "SetSpeed 30", "Can not change speed to 30\n");
 
 
 
-	/* Тормозим машину */
+	/* РўРѕСЂРјРѕР·РёРј РјР°С€РёРЅСѓ */
 	VerifyCommand(remoteControl, input, output, "SetSpeed 0", "Speed is changed to 0\n");
 
 
-	/* Выключаем двигатель и проверяем сообщение */
+	/* Р’С‹РєР»СЋС‡Р°РµРј РґРІРёРіР°С‚РµР»СЊ Рё РїСЂРѕРІРµСЂСЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ */
 	VerifyCommand(remoteControl, input, output, "EngineOff", "Engine is turned off\n");
 
-	/* Проверяем состояние машины */
+	/* РџСЂРѕРІРµСЂСЏРµРј СЃРѕСЃС‚РѕСЏРЅРёРµ РјР°С€РёРЅС‹ */
 	VerifyCommand(remoteControl, input, output, "Info", "Car info:\n\tEngine state: Off\n\tDirection: Not moving\n\tSpeed: 0\n\tGear: 0\n");
 
 
-	/* Пытаемся выполнить недопустимую команду */
+	/* РџС‹С‚Р°РµРјСЃСЏ РІС‹РїРѕР»РЅРёС‚СЊ РЅРµРґРѕРїСѓСЃС‚РёРјСѓСЋ РєРѕРјР°РЅРґСѓ */
 	VerifyCommand(remoteControl, input, output, "Fly", "Unknown command\n");
 
-	/* Проверяем состояние машины */
+	/* РџСЂРѕРІРµСЂСЏРµРј СЃРѕСЃС‚РѕСЏРЅРёРµ РјР°С€РёРЅС‹ */
 	VerifyCommand(remoteControl, input, output, "Info", "Car info:\n\tEngine state: Off\n\tDirection: Not moving\n\tSpeed: 0\n\tGear: 0\n");
 }
